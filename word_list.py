2f0fa7b0ce6807eff7b7e9906c8063254ed33a61
BaderShalata <99408220+BaderShalata@users.noreply.github.com>
2023-06-17 14:38:32 +0300

    Update and rename Transposing.py to word_list.py

---
+++ word_list.py
@@ -0,0 +1,6 @@
+wording = [
+    "apple", "banana", "orange", "grape", "watermelon", "pineapple",
+    "strawberry", "kiwi", "mango", "peach", "pear", "plum",
+    "blueberry", "cherry", "raspberry", "lemon", "lime", "coconut"
+]
+
